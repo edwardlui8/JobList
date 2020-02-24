7f14ea58229193d387e4388ebc8f37d5cd221c51
Edward <Edward@DESKTOP-09V4OT2>
2020-02-24 03:08:39 +0000

    Add project files.

---
+++ Job.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobList
+{
+    public class Job
+    {
+        private string title;
+        private string link;
+        private string salary;
+        private string date;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Link
+        {
+            get
+            {
+                return link;
+            }
+            set
+            {
+                link = value;
+            }
+        }
+
+        public string Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+
+        public string Date
+        {
+            get
+            {
+                return date;
+            }
+            set
+            {
+                date = value;
+            }
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace JobList
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Job> jobs = new ObservableCollection<Job>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.indeed.co.uk/jobs?q=graduate%20software%20engineer&l=South%20East%20London,%20Greater%20London&radius=10&sort=date");
+
+            var cards = doc.DocumentNode.SelectNodes("//a[@data-tn-element='jobTitle']");
+            foreach(var card in cards)
+            {
+                string href = card.Attributes["href"].Value;
+                jobs.Add(new Job { Title = card.InnerText, Link = "https://www.indeed.co.uk" + href });
+                }
+        }
+
+        public ObservableCollection<Job> Jobs
+        {
+            get
+            {
+                return jobs;
+            }
+        }
+
+        private void Link_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as HyperlinkButton;
+            if (button != null)
+            {
+                string url = @button.Content as string;
+                OpenUri(new Uri(url));
+            }
+        }
+
+        private async void OpenUri(Uri uri)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(uri);
+        }
+    }
+}
